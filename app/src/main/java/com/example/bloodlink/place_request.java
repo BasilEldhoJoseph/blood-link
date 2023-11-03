87548fc8abb416c732d406ff3e42b30e13696c0c
Basil_Eldho_Joseph <basil.eldho1414@gmail.com>
2023-11-03 23:21:53 +0530

    base
    3 files
    
    1.main activit::  registration
    2.request_list::  list of requests
    3.place_request::   user enter details to place request

---
+++ app/src/main/java/com/example/bloodlink/place_request.java
@@ -0,0 +1,14 @@
+package com.example.bloodlink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class place_request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_request);
+    }
+}
