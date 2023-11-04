333c43feca92478d06bdad06fcd94f8f3e72d79f
Abhinavsobi <146966128+Abhinavsobi@users.noreply.github.com>
2023-11-04 21:20:10 +0530

    initial phase done. page 1 finished.

--- app/src/main/java/com/example/bloodlink/MainActivity.java
+++ app/src/main/java/com/example/bloodlink/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.bloodlink;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
 
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView namereq = (TextView) findViewById(R.id.namereq);
+        TextView phonereq = (TextView) findViewById(R.id.phonereq);
+        //CREATE VARIABLES TO ACCEPT THE STRING FROM EDIT TEXT (MAY OR MAY NOT BE IN STRING FORMAT)
+        EditText editname =(EditText) findViewById(R.id.name);
+        EditText editphone =(EditText) findViewById(R.id.phone);
+        MaterialButton register =(MaterialButton) findViewById(R.id.register);
+        //ON BUTTON CLICK
+        register.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                //EDITTEXT ACCEPTED STRINGS CHANGES TO STRING FORMAT
+                String name = editname.getText().toString();
+                String phone1 = editphone.getText().toString();
+                //CONDITIONS FOR WHETHER DATA IF GIVEN OR NOT OF NOT GIVEN DISPLAY ERROR
+                if(name.equals("") || phone1.equals("")) {
+                    if(name.equals("")==true) {
+                        namereq.setText("  * pls enter your name");
+                    }
+                    else {
+                        namereq.setText("");
+                    }
+                    if(phone1.equals("")==true) {
+                        phonereq.setText("  * pls enter your number");
+                    }
+                    else {
+                        phonereq.setText("");
+                    }
+                    Toast.makeText(getApplicationContext(),"ERROR",Toast.LENGTH_SHORT).show();
+                }
+                //IF ALL CONDITIONS MET GO TO MAIN ACTIVITY 2
+                else {
+                    Intent next = new Intent(MainActivity.this, request_list.class);
+                    //SENDING DATA TO MAIN ACTIVITY 2
+                    next.putExtra("key1", name);
+                    next.putExtra("key2", phone1);
+                    startActivity(next);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bloodlink/request_list.java
+++ app/src/main/java/com/example/bloodlink/request_list.java
@@ -3,6 +3,7 @@ package com.example.bloodlink;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class request_list extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class request_list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_list);
+        //CREATE VARIABLES FOR MAKING CHANGES FOR TEXT VIEW
+        TextView name1 = (TextView) findViewById(R.id.name1);
+        TextView phone1 =(TextView) findViewById(R.id.phone1);
+        //ACCEPT DATA FROM MAIN ACTIVITY
+        Bundle extras = getIntent().getExtras();
+        if(extras!=null){
+            String name= extras.getString("key1");
+            name1.setText(name);
+            String phone= extras.getString("key2");
+            phone1.setText(phone);
+        }
+
     }
 }
